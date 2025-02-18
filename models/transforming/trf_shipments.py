a9d10aa1f32b4125ac341d527d66ac63dc3d96c3
Sathish Murugesan <sathishkumar.murugesan2@cognizant.com>
2025-02-18 05:48:16 +0000

    session14

--- models/transforming/trf_shipments.py
+++ models/transforming/trf_shipments.py
@@ -9,8 +9,7 @@ def model(dbt, session):
     shippers_df = dbt.ref('lkp_shippers')
 
     final_shipments_df = (
-                            shipments_df
-                            .join(shippers_df, shipments_df.shipperid == shippers_df.shipperid, 'left')
+                            shipments_df.join(shippers_df, shipments_df.shipperid == shippers_df.shipperid, 'left')
                             .select(
                                      shipments_df.orderid,
                                      shipments_df.lineno,
