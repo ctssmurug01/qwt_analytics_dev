88c223970ef357cf464e019ba7b6a24830bc5239
Sathish Murugesan <sathishkumar.murugesan2@cognizant.com>
2025-02-25 04:07:11 +0000

    Session 19 interim

--- models/transforming/trf_dates.sql
+++ models/transforming/trf_dates.sql
@@ -1,4 +1,4 @@
-{{config(materialized ='table',schema = 'transforming_dev')}}
+{{config(materialized ='table',schema = env_var('DBT_TRANSFORMSCHEMA','TRANSFORMING_DEV'))}}
 
 {% set min_orderdate = get_min_orderdate() %}
 {% set max_orderdate = get_max_orderdate() %}
